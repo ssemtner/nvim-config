c4166c752d68a20a09a32bd8953113bb3363bd4c
Scott Semtner <sjsemtner@gmail.com>
2022-12-17 00:44:47 -0800

    back to basic colors

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,9 +1,14 @@
-function ColorMyPencils(color)
+function SetColor(color)
 	color = color or "rose-pine"
 	vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
-ColorMyPencils()
+function SC(color)
+    SetColor(color)
+end
+
+SetColor()
+
